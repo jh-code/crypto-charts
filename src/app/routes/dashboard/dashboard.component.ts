e8c16fd268b1ef695ce8f1390b34c239331202d9
jh-code <jh.wphax@gmail.com>
2018-12-16 22:48:11 -0600

    feat: after fetching products, select first product
    products select box uses `valueChange` instead of `selectionChange` now

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { AVAILABLE_EXCHANGES } from '../../app.constants';
 import { Store, Select } from '@ngxs/store';
 import { FetchProducts, SubscribeProduct, FetchProductTicker, FetchProductTrades, CloseSocket } from '../../actions/trade.actions';
 import { TradeState } from '../../state/trade.state';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CoinbaseProProduct } from '../../models/coinbase-pro';
 import { MatSelectChange } from '@angular/material';
 import { tap } from 'rxjs/operators';
@@ -15,8 +15,11 @@ import { tap } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit, OnDestroy {
+  private productSub: Subscription;
+
   // TODO: change this to support other exchanges...
   public exchanges: string[] = AVAILABLE_EXCHANGES;
+  public products: CoinbaseProProduct[] = [];
   public selectedExchange = 'Coinbase Pro'; // TODO: support more exchanges
   public selectedProduct;
 
@@ -38,19 +41,28 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.store.dispatch([
       new FetchProducts()
     ]);
+
+    this.productSub = this.products$
+      .subscribe((products: CoinbaseProProduct[]) => {
+        if (products.length) {
+          this.products = products;
+          this.selectedProduct = products[0];
+          this.selectProduct(this.selectedProduct);
+        }
+      });
   }
 
-  public _selectProduct(event: MatSelectChange): void {
-    // this.store.dispatch(new SubscribeProduct(event.value));
+  public selectProduct(product: CoinbaseProProduct): void {
     this.store.dispatch([
-      new FetchProductTicker(event.value),
-      new FetchProductTrades(event.value),
-      new SubscribeProduct(event.value)
+      new FetchProductTicker(product),
+      new FetchProductTrades(product),
+      new SubscribeProduct(product)
     ]);
   }
 
   public ngOnDestroy(): void {
     this.store.dispatch(new CloseSocket());
+    this.productSub.unsubscribe();
   }
 
 }
